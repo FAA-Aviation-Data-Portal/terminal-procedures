1de1d1ed042fe93f7471ed69431eaa7fe24dc5ed
Chad.March <cmarch@resdat.com>
2020-10-27 14:48:17 -0700

    New methods to get next cycle when available

--- example.js
+++ example.js
@@ -1,15 +1,57 @@
 const terminalProcedures = require('./')
 
-terminalProcedures.fetchCurrentCycle().then(r => console.log(r))
+// Should log current cycle effective object
+terminalProcedures.fetchCycle().then(c => {
+  console.log('current cycle effective object, no param')
+  console.dir(c)
+})
 
-terminalProcedures.currentCycleEffectiveDates().then(console.log)
+// Should log current cycle effective object
+terminalProcedures.fetchCycle('Current').then(c => {
+  console.log('current cycle effective object, with param')
+  console.dir(c)
+})
+
+// Should log next cycle effective object
+terminalProcedures.fetchCycle('Next').then(c => {
+  console.log('next cycle effective object, with param')
+  console.dir(c)
+})
+
+// Should log current cycle code
+terminalProcedures.fetchCurrentCycleCode().then(c => {
+  console.log('current cycle code')
+  console.log(c)
+})
+
+// Should log the current cycle effective dates
+terminalProcedures.getCycleEffectiveDates().then(c => {
+  console.log('the current cycle effective dates, no param')
+  console.log(c)
+})
+// Should log the current cycle effective dates
+terminalProcedures.getCycleEffectiveDates('Current').then(c => {
+  console.log('the current cycle effective dates, with param')
+  console.log(c)
+})
+// Should log the next cycle effective dates
+terminalProcedures.getCycleEffectiveDates('Next').then(c => {
+  console.log('the next cycle effective dates')
+  console.log(c)
+})
+
+// Should log the current cycle effective dates
+terminalProcedures.currentCycleEffectiveDates().then(c => {
+  console.log('the current cycle effective dates')
+  console.log(c)
+})
 
 // Also try updateing the Flag property to include one or more of the following:
 //  A for only those that were Added since the last effective date
 //  C for only those that were Changed since the last effective date
 //  D for only those that were Deleted since the last effective date
 //  Leave empty to get all regardless of if they've been added or changed
-terminalProcedures.list('PANC', { flag: [ 'A', 'C' ], }).then(results => {
+terminalProcedures.list('PANC', { flag: [ ], }).then(results => {
   console.log(results)
   const out = results.map(tp => {
     return {
